3d753e6c66f32cc0ce443e6824b73863b47aff96
MrOpser <130372751+MrOpser@users.noreply.github.com>
2023-05-26 12:10:18 +0300

    Update Code_Wars_duble_of_array.py

--- Code_Wars_duble_of_array.py
+++ Code_Wars_duble_of_array.py
@@ -5,17 +5,21 @@
 # [1, 2, 3] --> [2, 4, 6]
 
 def duble_array(array: list[int]) -> list[int]:
-    res_array = array
-    count = 0
-    for i in array:
-        element = i * 2
-        res_array[count] = element
-        count += 1
-    return print(res_array)
+    res_array = array  # equalate the lists to each others
+    count = 0  # declare counter of number of elements
+    for i in array:  # for i elements
+        element = i * 2  # double it
+        res_array[count] = element  # assigned to variables
+        count += 1  # increase counter on one
+    return print(res_array)  # see result
 
 
 if __name__ == '__main__':
     duble_array([2, 3, 9, 234])
+
+
+
+
 
 
 
